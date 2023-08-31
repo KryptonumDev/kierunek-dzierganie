9c87a78e5e390abeb5e519bbde8c8d376e514ad8
Bogumił <hello@milewski.me>
2023-08-31 16:05:23 +0200

    added facebook pixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -46,6 +46,23 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-F5CD13WL6R');
           `}
         </Script>
+        <Script id="fb-pixel">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '106002735936658');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style="display:none" alt="" src="https://www.facebook.com/tr?id=106002735936658&ev=PageView&noscript=1" />
+        </noscript>
       </body>
     </html>
   )
