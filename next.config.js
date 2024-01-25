ba7092943dffe851f79daf47ed9f210d5e1bb9ff
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-01-25 15:25:48 +0100

    remove sassOptions in the nextConfig

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const path = require('path');
 
 const nextConfig = {
   productionBrowserSourceMaps: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
