6d4bd98a02ed5bc99a54f64939dd5571695c85e1
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-02-07 16:04:56 +0100

    added animations and Icons to TabSection

--- src/components/_global/TabSection/TabSection.tsx
+++ src/components/_global/TabSection/TabSection.tsx
@@ -3,6 +3,7 @@ import Button from '@/components/ui/Button';
 import styles from './TabSection.module.scss';
 import type { Props } from './TabSection.types';
 import Tabs from './_Tabs';
+import { Decoration1, Decoration2, Decoration3, Decoration4, Decoration5 } from './_Icons';
 
 const TabSection = ({ heading, paragraph, list, cta }: Props) => {
   const renderedList = list.map(({ title, description }) => ({
@@ -16,7 +17,10 @@ const TabSection = ({ heading, paragraph, list, cta }: Props) => {
         <Markdown.h2>{heading}</Markdown.h2>
         {paragraph && <Markdown className={styles.paragraph}>{paragraph}</Markdown>}
       </header>
-      <Tabs list={renderedList} />
+      <Tabs
+        list={renderedList}
+        icons={[ Decoration1, Decoration2, Decoration3, Decoration4, Decoration5 ]}
+      />
       {cta && (
         <Button
           data={cta}
--- src/components/_global/TabSection/_Tabs.tsx
+++ src/components/_global/TabSection/_Tabs.tsx
@@ -1,16 +1,18 @@
 'use client';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import styles from './TabSection.module.scss';
+import { easing } from '@/global/constants';
 
 type Props = {
   list: {
     title: string;
     description: React.ReactNode;
   }[];
+  icons: React.ReactNode[];
 };
 
-const Tabs = ({ list }: Props) => {
+const Tabs = ({ list, icons }: Props) => {
   const [tab, setTab] = useState(0);
 
   return (
@@ -22,7 +24,24 @@ const Tabs = ({ list }: Props) => {
             aria-current={tab === i}
             onClick={() => setTab(i)}
           >
-            {title}
+            <motion.div
+              layout
+              initial={{ width: i === 0 ? 'auto' : 0 }}
+              animate={{
+                width: tab === i ? 'auto' : 0,
+              }}
+              exit={{ width: 0 }}
+              style={{
+                overflow: 'hidden',
+              }}
+              transition={{
+                duration: 0.5,
+                ease: easing,
+              }}
+            >
+              {icons[i]}
+            </motion.div>
+            <span>{title}</span>
           </button>
         ))}
       </div>
@@ -34,7 +53,7 @@ const Tabs = ({ list }: Props) => {
               opacity: tab === i ? 1 : 0,
             }}
             animate={{ opacity: tab === i ? 1 : 0 }}
-            transition={{ duration: .5 }}
+            transition={{ duration: 0.5 }}
             className={styles.item}
             key={i}
           >
