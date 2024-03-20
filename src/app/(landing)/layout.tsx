4d6abea947fe4ce39901fdffbeefce77cf08a3c9
Shembou <sshembou@gmail.com>
2024-03-20 13:32:23 +0100

    added SchemaOrganization to project

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,9 +1,13 @@
 import Nav from '@/components/_landing/Header';
 import Footer from '@/components/_landing/Footer';
+import SchemaOrganization from '@/global/Schema/Ogranization';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <head>
+        <SchemaOrganization />
+      </head>
       <Nav />
       <main id='main'>{children}</main>
       <Footer />
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,10 +2,14 @@ import { Toaster } from 'react-hot-toast';
 import Header from '@/components/_global/Header';
 import Footer from '@/components/_global/Footer';
 import CartProvider from 'src/HOCs/cart-provider';
+import SchemaOrganization from '@/global/Schema/Ogranization';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <head>
+        <SchemaOrganization />
+      </head>
       <CartProvider>
         <Header />
         <main id='main'>{children}</main>
