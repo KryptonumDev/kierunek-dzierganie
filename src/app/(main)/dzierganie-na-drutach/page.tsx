9df23e152146513f287690a862998c14bc4dc089
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-25 11:07:10 +0100

    Added StepsGrid component

--- src/app/(main)/dzierganie-na-drutach/page.tsx
+++ src/app/(main)/dzierganie-na-drutach/page.tsx
@@ -1,28 +1,32 @@
 import sanityFetch from '@/utils/sanity.fetch';
 import { QueryMetadata } from '@/global/Seo/query-metadata';
 import Breadcrumbs from '@/components/_global/Breadcrumbs';
-import Components, { Components_Query } from '@/components/Components';
-import type { PageQueryProps } from '@/global/types';
+import HeroSimple, { HeroSimple_Query } from '@/components/_global/HeroSimple';
+import type { KnittingPage_QueryTypes } from './page.types';
+import StepsGrid, { StepsGrid_Query } from '@/components/_global/StepsGrid';
 
 const page = { name: 'Dzierganie na drutach', path: '/dzierganie-na-drutach' };
 
 const KnittingPage = async () => {
-  const { content } = await query();
+  const { HeroSimple: HeroSimpleData, StepsGrid: StepsGridData } = await query();
 
   return (
     <>
       <Breadcrumbs data={[page]} />
-      <Components data={content} />
+      <HeroSimple {...HeroSimpleData} />
+      <StepsGrid {...StepsGridData} />
     </>
   );
 };
+
 export default KnittingPage;
 
-const query = async (): Promise<PageQueryProps> => {
-  return await sanityFetch<PageQueryProps>({
+const query = async (): Promise<KnittingPage_QueryTypes> => {
+  return await sanityFetch<KnittingPage_QueryTypes>({
     query: /* groq */ `
       *[_type == "Knitting_Page"][0] {
-        ${Components_Query}
+        ${HeroSimple_Query}
+        ${StepsGrid_Query}
       }
     `,
     tags: ['Knitting_Page'],
