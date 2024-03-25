97a7d1313ab865c1cbf84df354e53ed2ad1cef19
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-25 12:45:09 +0100

    added LatestBlogEntries to CrochetingPage

--- src/app/(main)/szydelkowanie/page.tsx
+++ src/app/(main)/szydelkowanie/page.tsx
@@ -2,19 +2,25 @@ import sanityFetch from '@/utils/sanity.fetch';
 import { QueryMetadata } from '@/global/Seo/query-metadata';
 import Breadcrumbs from '@/components/_global/Breadcrumbs';
 import HeroSimple, { HeroSimple_Query } from '@/components/_global/HeroSimple';
-import type { CrochetingPage_QueryTypes } from './page.types';
 import StepsGrid, { StepsGrid_Query } from '@/components/_global/StepsGrid';
+import LatestBlogEntries, { LatestBlogEntries_Query } from '@/components/_global/LatestBlogEntries';
+import type { CrochetingPage_QueryTypes } from './page.types';
 
 const page = { name: 'Szydełkowanie', path: '/szydelkowanie' };
 
 const CrochetingPage = async () => {
-  const { HeroSimple: HeroSimpleData, StepsGrid: StepsGridData } = await query();
+  const {
+    HeroSimple: HeroSimpleData,
+    StepsGrid: StepsGridData,
+    LatestBlogEntries: LatestBlogEntriesData,
+  } = await query();
 
   return (
     <>
       <Breadcrumbs data={[page]} />
       <HeroSimple {...HeroSimpleData} />
       <StepsGrid {...StepsGridData} />
+      <LatestBlogEntries {...LatestBlogEntriesData} />
     </>
   );
 };
@@ -27,6 +33,7 @@ const query = async (): Promise<CrochetingPage_QueryTypes> => {
       *[_type == "Crocheting_Page"][0] {
         ${HeroSimple_Query}
         ${StepsGrid_Query}
+        ${LatestBlogEntries_Query(true)}
       }
     `,
     tags: ['Crocheting_Page'],
