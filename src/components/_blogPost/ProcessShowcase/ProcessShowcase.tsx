cca76bf57403069c062d440bbaa0c8d330ee3c08
Shembou <sshembou@gmail.com>
2024-03-26 14:41:16 +0100

    updated ProcessShowcase and added styles

--- src/components/_blogPost/ProcessShowcase/ProcessShowcase.tsx
+++ src/components/_blogPost/ProcessShowcase/ProcessShowcase.tsx
@@ -1,9 +1,35 @@
+import Img from '@/components/ui/image';
 import styles from './ProcessShowcase.module.scss';
 import type { ProcessShowcaseTypes } from './ProcessShowcase.types';
+import Markdown from '@/components/ui/markdown';
 
 const ProcessShowcase = ({ list }: ProcessShowcaseTypes) => {
-  console.log(list);
-  return <section className={styles['ProcessShowcase']}></section>;
+  return (
+    <div className={styles['ProcessShowcase']}>
+      {list.map(({ heading, process }, index) => (
+        <div
+          key={index}
+          className={styles.item}
+        >
+          <Markdown.h2 className={styles.heading}>{heading}</Markdown.h2>
+          <div className={styles.process}>
+            {process.map(({ img, paragraph }, index) => (
+              <div
+                key={index}
+                className={styles.processItem}
+              >
+                <Img
+                  data={img}
+                  sizes=''
+                />
+                <Markdown>{paragraph}</Markdown>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ProcessShowcase;
