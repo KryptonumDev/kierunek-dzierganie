1c6c2395dc9b8f552323ed353d8bcb401fea17ba
Shembou <sshembou@gmail.com>
2024-03-27 13:06:45 +0100

    added highlightedImage

--- src/components/_blogPost/HighlightedImage/HighlightedImage.tsx
+++ src/components/_blogPost/HighlightedImage/HighlightedImage.tsx
@@ -1,9 +1,22 @@
+import Img from '@/components/ui/image';
 import styles from './HighlightedImage.module.scss';
 import type { HighlightedImageTypes } from './HighlightedImage.types';
+import Markdown from '@/components/ui/markdown';
 
 const HighlightedImage = ({ img, isBackground, paragraph }: HighlightedImageTypes) => {
   console.log(img, isBackground, paragraph);
-  return <section className={styles['HighlightedImage']}></section>;
+  return (
+    <div
+      className={styles['HighlightedImage']}
+      data-isBackground={isBackground}
+    >
+      <Img
+        data={img}
+        sizes=''
+      />
+      <Markdown>{paragraph}</Markdown>
+    </div>
+  );
 };
 
 export default HighlightedImage;
