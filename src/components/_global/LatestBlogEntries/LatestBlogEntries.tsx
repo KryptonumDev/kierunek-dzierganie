61820bcb9cbde7b5b0a5ff00fd74f696e8c652f6
Shembou <sshembou@gmail.com>
2024-04-11 08:55:19 +0200

    feat: added LatestBlogEntries

--- src/components/_global/LatestBlogEntries/LatestBlogEntries.tsx
+++ src/components/_global/LatestBlogEntries/LatestBlogEntries.tsx
@@ -1,14 +1,36 @@
 import Markdown from '@/components/ui/markdown';
 import styles from './LatestBlogEntries.module.scss';
 import type { LatestBlogEntriesTypes } from './LatestBlogEntries.types';
+import Img from '@/components/ui/image';
+import Link from 'next/link';
 
-const LatestBlogEntries = ({ heading, paragraph }: LatestBlogEntriesTypes) => {
+const LatestBlogEntries = ({ heading, paragraph, entries }: LatestBlogEntriesTypes) => {
   return (
     <section className={styles['LatestBlogEntries']}>
       <header>
         <Markdown.h2>{heading}</Markdown.h2>
         <Markdown>{paragraph}</Markdown>
       </header>
+      <div className={styles.blogPosts}>
+        {entries.map(({ hero: { heading, img, paragraph }, slug }, index) => {
+          return (
+            <Link
+              key={index}
+              className={styles.entry}
+              href={`/blog/${slug}`}
+            >
+              <Img
+                data={img}
+                sizes='(max-width: 499px) 100vw, (max-width: 999px) 50vw, 33vw'
+              />
+              <div className={styles.text}>
+                <Markdown.h3>{heading}</Markdown.h3>
+                <Markdown>{paragraph}</Markdown>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </section>
   );
 };
--- src/components/_global/LatestBlogEntries/LatestBlogEntries.types.ts
+++ src/components/_global/LatestBlogEntries/LatestBlogEntries.types.ts
@@ -1,4 +1,14 @@
+import { type ImgType } from '@/global/types';
+
 export type LatestBlogEntriesTypes = {
   heading: string;
   paragraph: string;
+  entries: {
+    hero: {
+      heading: string;
+      paragraph: string;
+      img: ImgType;
+    };
+    slug: string;
+  }[];
 };
