c2ab617fe95c9d49de636430b5f2ee4f10b5b590
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-04-15 11:36:35 +0200

    fixed Height in TilesSticky Header

--- src/components/_global/TilesSticky/TilesSticky.tsx
+++ src/components/_global/TilesSticky/TilesSticky.tsx
@@ -9,13 +9,14 @@ const TilesSticky = ({ heading, paragraph, cta, list }: Props) => {
       <header>
         <Markdown.h2>{heading}</Markdown.h2>
         <Markdown>{paragraph}</Markdown>
-        {cta && (
-          <Button data={cta} />
-        )}
+        {cta && <Button data={cta} />}
       </header>
       <div className={styles.wrapper}>
         {list.map(({ heading, paragraph }, i) => (
-          <div className={styles.item} key={i}>
+          <div
+            className={styles.item}
+            key={i}
+          >
             <Markdown.h3>{heading}</Markdown.h3>
             <Markdown>{paragraph}</Markdown>
           </div>
