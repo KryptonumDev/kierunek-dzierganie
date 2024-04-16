e7eddccd809a9385142af0b6eb069db4d2d2235c
kryptonumstudio <kryptonumstudio@gmail.com>
2024-04-16 21:56:07 +0200

    start product page refactor

--- src/app/(main)/(szydelkowanie)/kursy-szydelkowania/[slug]/page.tsx
+++ src/app/(main)/(szydelkowanie)/kursy-szydelkowania/[slug]/page.tsx
@@ -2,7 +2,6 @@ import { notFound } from 'next/navigation';
 import sanityFetch from '@/utils/sanity.fetch';
 import { QueryMetadata } from '@/global/Seo/query-metadata';
 import Breadcrumbs from '@/components/_global/Breadcrumbs';
-import Parameters from '@/components/_product/Parameters';
 import Informations from '@/components/_product/Informations';
 import Description, { Description_Query } from '@/components/_product/Description';
 import TableOfContent from '@/components/_product/TableOfContent';
@@ -10,6 +9,7 @@ import type { ProductPageQueryProps } from '@/global/types';
 import Package, { Package_Query } from '@/components/_product/Package';
 import { PRODUCT_CARD_QUERY } from '@/global/constants';
 import Reviews from '@/components/_product/Reviews';
+import HeroVirtual from '@/components/_product/HeroVirtual';
 
 const Product = async ({ params: { slug } }: { params: { slug: string } }) => {
   const {
@@ -25,7 +25,9 @@ const Product = async ({ params: { slug } }: { params: { slug: string } }) => {
       // gallery,
       parameters,
       description,
-      course,
+      chapters,
+      reviews,
+      rating,
       courses,
     },
     card,
@@ -46,8 +48,20 @@ const Product = async ({ params: { slug } }: { params: { slug: string } }) => {
         ]}
         visible={true}
       />
+      <HeroVirtual />
+
+
+      {/* 
+        TODO: add section you can buy in bundle
+      <Package
+        product={card}
+        heading={'Jeden pakiet – niezliczona ilość wiedzy'}
+        paragraph={'Zdobądź niezbędne umiejętności i rozwijaj kreatywność z **pakietem kursów** – w korzystnej cenie!'}
+        courses={courses}
+      /> */}
+
       <Informations tabs={['Spis treści', 'Pakiet', 'Opis', 'Parametry', 'Opinie']}>
-        {course && <TableOfContent chapters={course.chapters} />}
+        {chapters && <TableOfContent chapters={chapters} />}
         {courses && (
           <Package
             product={card}
@@ -59,8 +73,7 @@ const Product = async ({ params: { slug } }: { params: { slug: string } }) => {
           />
         )}
         {description?.length > 0 && <Description data={description} />}
-        {parameters?.length > 0 && <Parameters parameters={parameters} />}
-        {course?.reviews && course?.reviews.length > 0 && <Reviews reviews={course.reviews} />}
+        {reviews.length > 0 && <Reviews reviews={reviews} />}
       </Informations>
       {/* TODO: Add featured courses */}
     </>
@@ -70,7 +83,7 @@ const Product = async ({ params: { slug } }: { params: { slug: string } }) => {
 export default Product;
 
 export async function generateMetadata({ params: { slug } }: { params: { slug: string } }) {
-  return await QueryMetadata('product', `/kursy-szydelkowania/${slug}`, slug);
+  return await QueryMetadata('course', `/kursy-szydelkowania/${slug}`, slug);
 }
 
 const query = async (slug: string): Promise<ProductPageQueryProps> => {
@@ -102,51 +115,20 @@ const query = async (slug: string): Promise<ProductPageQueryProps> => {
         },
         ${Package_Query}
         ${Description_Query}
-        course -> {
-          chapters[] {
-            chapterName,
-            lessons[] -> {
-              title,
-              lengthInMinutes,
-            },
+        chapters[] {
+          chapterName,
+          lessons[] -> {
+            title,
+            lengthInMinutes,
           },
-          "reviews": *[_type == 'courseReviewCollection' && references(^._id)][0...10]{
-            rating,
-            review,
-            nameOfReviewer,
-            _id
-          },
-          "rating": math::avg(*[_type == 'courseReviewCollection' && references(^._id)]{rating}.rating)
         },
-        parameters[]{
-          name,
-          value,
+        "reviews": *[_type == 'productReviewCollection' && references(^._id)][0...10]{
+          rating,
+          review,
+          nameOfReviewer,
+          _id
         },
-        variants[]{
-          name,
-          price,
-          discount,
-          countInStock,
-          featuredVideo,
-          gallery[]{
-            asset -> {
-              url,
-              altText,
-              metadata {
-                lqip,
-                dimensions {
-                  width,
-                  height,
-                }
-              }
-            }
-          },
-          attributes[]{
-            type,
-            name,
-            value
-          }
-        },
+        "rating": math::avg(*[_type == 'productReviewCollection' && references(^._id)]{rating}.rating)
       },
       "card": *[(_type == 'course' || _type == 'bundle') && basis == 'crocheting' && slug.current == $slug][0] {
         ${PRODUCT_CARD_QUERY}
@@ -157,7 +139,7 @@ const query = async (slug: string): Promise<ProductPageQueryProps> => {
     tags: ['product'],
   });
 
-  !data?.product && notFound();
+  !data?.product?._id && notFound();
   return data;
 };
 
