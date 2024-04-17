b554a2124491252839fc4080509c76a0194fe6b6
Shembou <sshembou@gmail.com>
2024-04-17 13:12:04 +0200

    fix: fixed grammar error

--- src/components/_global/ProductsListing/ProductsListing.tsx
+++ src/components/_global/ProductsListing/ProductsListing.tsx
@@ -44,7 +44,7 @@ const ProductsListing = ({
         </div>
       </div>
       {/* TODO: Change no items text */}
-      {products.length === 0 && <h2>Niestety teraz w tym rozdziale nic niema :( </h2>}
+      {products.length === 0 && <h2>Niestety teraz w tym rozdziale nic nie ma :( </h2>}
       {productsTotalCount > 10 && (
         <Pagination
           basis={basis}
