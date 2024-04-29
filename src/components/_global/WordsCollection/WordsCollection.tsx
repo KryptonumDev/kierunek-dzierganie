c1bc174819879dff412c48f02b193e90a74e3b52
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-04-29 12:39:05 +0200

    changed layout in WordsCollection

--- src/components/_global/WordsCollection/WordsCollection.tsx
+++ src/components/_global/WordsCollection/WordsCollection.tsx
@@ -2,22 +2,22 @@ import Markdown from '@/components/ui/markdown';
 import Button from '@/components/ui/Button';
 import styles from './WordsCollection.module.scss';
 import type { Props } from './WordsCollection.types';
-import List from './_List';
 
 const WordsCollection = ({ heading, list, cta, index }: Props) => {
-  const HeadingComponenet = index === 0 ? Markdown.h1 : Markdown.h2;
-  const renderedList = list.map((item) => ({
-    text: item,
-    marginLeft: `${Math.floor(Math.random() * 39)}%`,
-    y: Math.random() * -96,
-  }));
+  const Heading = index === 0 ? Markdown.h1 : Markdown.h2;
 
   return (
     <section className={styles['WordsCollection']}>
       <header>
-        <HeadingComponenet>{heading}</HeadingComponenet>
+        <Heading>{heading}</Heading>
       </header>
-      <List list={renderedList} />
+      <ul className={styles.list}>
+        {list.map((item, i) => (
+          <li key={i} className={styles.item}>
+            {item}
+          </li>
+        ))}
+      </ul>
       {cta && (
         <Button
           data={cta}
