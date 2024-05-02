00572ef96998ddc2f905da536559c9c8b18cd9d9
kryptonumstudio <kryptonumstudio@gmail.com>
2024-05-02 15:17:51 +0200

    fix

--- src/emails/CompleteOrder/index.ts
+++
@@ -1,9 +0,0 @@
-import CompleteOrder from './CompleteOrder';
-export default CompleteOrder;
-export type { CompleteOrderTypes } from './CompleteOrder.types';
-
-export const CompleteOrder_Query = `
-  _type == "CompleteOrder" => {
-
-  },
-`;
