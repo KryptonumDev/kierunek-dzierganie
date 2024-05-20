0c90d5803cc062a031ecab370ba58d6caad38a85
kryptonumstudio <kryptonumstudio@gmail.com>
2024-05-20 21:18:15 +0200

    fix

--- src/app/api/payment/complete/generate-bill.ts
+++ src/app/api/payment/complete/generate-bill.ts
@@ -113,11 +113,11 @@ export async function generateBill(data: any, id: string) {
   });
 
   const billId = await billRes.json();
-
+  console.log(billId.response.Identyfikator);
   await supabase
     .from('orders')
     .update({
-      bill: billId.response.Identyfikator,
+      bill: String(billId.response.Identyfikator),
       status: data.need_delivery ? 2 : 3,
     })
     .eq('id', id);
