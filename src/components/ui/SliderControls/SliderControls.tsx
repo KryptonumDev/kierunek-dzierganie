a3a7f37e13e600a0cf06943380353d804b652006
Shembou <sshembou@gmail.com>
2024-05-22 16:49:01 +0200

    added condition to navigation

--- src/components/ui/SliderControls/SliderControls.tsx
+++ src/components/ui/SliderControls/SliderControls.tsx
@@ -32,7 +32,7 @@ const useNavigation = (emblaApi: EmblaCarouselType | undefined) => {
     prevBtnDisabled,
     nextBtnDisabled,
     onPrevButtonClick,
-    onNextButtonClick
+    onNextButtonClick,
   };
 };
 
@@ -40,10 +40,13 @@ const usePagination = (emblaApi: EmblaCarouselType | undefined) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
 
-  const onDotButtonClick = useCallback((index: number) => {
-    if (!emblaApi) return;
-    emblaApi.scrollTo(index);
-  }, [emblaApi]);
+  const onDotButtonClick = useCallback(
+    (index: number) => {
+      if (!emblaApi) return;
+      emblaApi.scrollTo(index);
+    },
+    [emblaApi]
+  );
 
   const onInit = useCallback((emblaApi: EmblaCarouselType) => {
     setScrollSnaps(emblaApi.scrollSnapList());
@@ -65,7 +68,7 @@ const usePagination = (emblaApi: EmblaCarouselType | undefined) => {
   return {
     selectedIndex,
     scrollSnaps,
-    onDotButtonClick
+    onDotButtonClick,
   };
 };
 
@@ -74,61 +77,67 @@ export default function SliderControls({ api }: { api: EmblaCarouselType | undef
   const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } = useNavigation(api);
 
   return (
-    <div className="embla__controls">
-      <button
-        className="embla__button embla__button--prev"
-        type="button"
-        onClick={onPrevButtonClick}
-        disabled={prevBtnDisabled}
-        aria-label='Przejdź do poprzedniego elementu'
-      >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width={23}
-          height={9}
-          viewBox='0 0 23 9'
-          fill='none'
-        >
-          <path
-            d='M22.906 4.767H1.094m0 0 3.75-3.75m-3.75 3.75 3.75 3.75'
-            stroke='#53423C'
-            strokeWidth={0.75}
-          />
-        </svg>
-      </button>
-      <div className="embla__dots">
-        {scrollSnaps.map((_, index) => (
-          <button
-            key={index}
-            type="button"
-            className='embla__dot'
-            onClick={() => onDotButtonClick(index)}
-            aria-current={index === selectedIndex}
-            aria-label={`Przejdź do ${index + 1} elementu`}
-          />
-        ))}
-      </div>
-      <button
-        className="embla__button embla__button--next"
-        type="button"
-        onClick={onNextButtonClick}
-        disabled={nextBtnDisabled}
-        aria-label='Przejdź do następnego elementu'
-      >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width={23}
-          height={9}
-          viewBox='0 0 23 9'
-          fill='none'
-        >
-          <path
-            d='M.094 4.767h21.812m0 0-3.75-3.75m3.75 3.75-3.75 3.75'
-            stroke='#53423C'
-            strokeWidth={0.75}
-          />
-        </svg>
-      </button>
-    </div>
+    <>
+      {scrollSnaps.length > 1 && (
+        <>
+          <div className='embla__controls'>
+            <button
+              className='embla__button embla__button--prev'
+              type='button'
+              onClick={onPrevButtonClick}
+              disabled={prevBtnDisabled}
+              aria-label='Przejdź do poprzedniego elementu'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width={23}
+                height={9}
+                viewBox='0 0 23 9'
+                fill='none'
+              >
+                <path
+                  d='M22.906 4.767H1.094m0 0 3.75-3.75m-3.75 3.75 3.75 3.75'
+                  stroke='#53423C'
+                  strokeWidth={0.75}
+                />
+              </svg>
+            </button>
+            <div className='embla__dots'>
+              {scrollSnaps.map((_, index) => (
+                <button
+                  key={index}
+                  type='button'
+                  className='embla__dot'
+                  onClick={() => onDotButtonClick(index)}
+                  aria-current={index === selectedIndex}
+                  aria-label={`Przejdź do ${index + 1} elementu`}
+                />
+              ))}
+            </div>
+            <button
+              className='embla__button embla__button--next'
+              type='button'
+              onClick={onNextButtonClick}
+              disabled={nextBtnDisabled}
+              aria-label='Przejdź do następnego elementu'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width={23}
+                height={9}
+                viewBox='0 0 23 9'
+                fill='none'
+              >
+                <path
+                  d='M.094 4.767h21.812m0 0-3.75-3.75m3.75 3.75-3.75 3.75'
+                  stroke='#53423C'
+                  strokeWidth={0.75}
+                />
+              </svg>
+            </button>
+          </div>
+        </>
+      )}
+    </>
   );
 }
