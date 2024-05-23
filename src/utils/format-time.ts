f0383b6f567d87f0e253ae6ada67779e5d556923
Shembou <sshembou@gmail.com>
2024-05-23 13:35:12 +0200

    fix: format-time script

--- src/utils/format-time.ts
+++ src/utils/format-time.ts
@@ -3,14 +3,21 @@ export const formatTime = (time: number) => {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.ceil((seconds % 3600) / 60);
 
+  const lastDigitOfHours = parseInt(hours.toString().slice(-1));
+
+  console.log(lastDigitOfHours);
+
   if (hours === 0) {
     return `${minutes} ${minutes === 1 ? 'minutę' : minutes < 5 ? 'minuty' : 'minut'}`;
   }
 
-  if (hours > 6) {
-    return `${hours} ${hours === 1 ? 'godzinę' : hours < 5 ? 'godziny' : 'godzin'}`;
+  if (hours > 0 && hours < 20) {
+    return `${hours} godzin`;
   }
 
+  if (hours >= 20) {
+    return `${hours} ${lastDigitOfHours === 1 ? 'godzin' : lastDigitOfHours < 5 && lastDigitOfHours != 0 ? 'godziny' : 'godzin'}`;
+  }
   if (minutes === 0) {
     return `${hours} ${hours === 1 ? 'godzinę' : hours < 5 ? 'godziny' : 'godzin'}`;
   }
