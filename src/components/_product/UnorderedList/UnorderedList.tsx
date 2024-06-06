0b3b11ee52f71c3e141bd48dd34b958732231aef
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-06-06 13:43:00 +0200

    Change UnorderedList list render option

--- src/components/_product/UnorderedList/UnorderedList.tsx
+++ src/components/_product/UnorderedList/UnorderedList.tsx
@@ -2,16 +2,13 @@ import Markdown from '@/components/ui/markdown';
 import styles from './UnorderedList.module.scss';
 import type { UnorderedListTypes } from './UnorderedList.types';
 
-const UnorderedList = ({ heading, paragraph, list }: UnorderedListTypes) => {
-  const formattedList = list.map((item) => `- ${item}`).join('\n');
-
+const UnorderedList = ({ heading, paragraph }: UnorderedListTypes) => {
   return (
     <section className={styles['UnorderedList']}>
       <header>
         <Markdown.h2>{heading}</Markdown.h2>
       </header>
       {paragraph && <Markdown className={styles.paragraph}>{paragraph}</Markdown>}
-      <Markdown className={styles.list}>{formattedList}</Markdown>
     </section>
   );
 };
