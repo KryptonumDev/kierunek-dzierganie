aec4a9e8509d0945ed0fa3598eab748b796815e2
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-10-29 12:15:26 +0100

    add new redirects

--- redirects.mjs
+++ redirects.mjs
@@ -25,105 +25,240 @@ export const redirects = [
     permanent: false,
   },
 
-
   {
     source: '/kursy-szydelkowania/materialy-do-kursu-podstaw-szydelkowania',
     destination: '/produkty-do-szydelkowania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-szydelkowania/pakiet-materialow-do-kursu-zyrafki',
     destination: '/produkty-do-szydelkowania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-dziergania-na-drutach/materialy-do-kursu-twoj-pierwszy-sweter-na-drutach',
     destination: '/produkty-do-dziergania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-szydelkowania/drukowana-instrukcja-do-kursu-szydelkowy-kotek',
     destination: '/kursy-szydelkowania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-dziergania-na-drutach/materialy-do-kursu-szydelkowe-bomb-k-owe-zwierzaki',
     destination: '/kursy-szydelkowania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-szydelkowania/drukowana-instrukcja-do-kursu-kocyk-merino',
     destination: '/kursy-szydelkowania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-szydelkowania/drukowana-instrukcja-do-kursu-swiateczne-ziomki',
     destination: '/kursy-szydelkowania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-szydelkowania/voucher-kurs-szydelkowy-piesek',
     destination: '/produkty-do-szydelkowania/voucher-upominkowy',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-szydelkowania/drukowana-instrukcja-do-kursu-nietoperz-karzan',
     destination: '/produkty-do-szydelkowania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-szydelkowania/pakiet-materialow-do-krolikow',
     destination: '/produkty-do-szydelkowania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-szydelkowania/kurs-nietoperz-karzan',
     destination: '/kursy-szydelkowania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-dziergania-na-drutach/woreczek-zrob-mi-mamo',
     destination: '/kursy-dziergania-na-drutach',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-szydelkowania/drukowana-instrukcja-do-kursu-szydelkowe-koszyczki',
     destination: '/kursy-szydelkowania/kurs-podstaw-szydelkowania-koszyczki',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-dziergania-na-drutach/zestawy-drutow-wymiennych-ginger-deluxe',
     destination: '/produkty-do-dziergania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/produkty-do-dziergania/agrafki-dziewiarskie-3szt',
     destination: '/produkty-do-dziergania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-dziergania-na-drutach/lniane-woreczki-na-robotke',
     destination: '/kursy-dziergania-na-drutach',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-szydelkowania/materialy-do-kursu-swiateczne-ziomki',
     destination: '/produkty-do-szydelkowania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/landing/wydziergaj-',
     destination: '/',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/kursy-szydelkowania/pakiet-materialow-do-kursu-podkladki',
     destination: '/produkty-do-szydelkowania',
-    permanent: true
+    permanent: true,
   },
   {
     source: '/o-mnie',
     destination: '/zespol',
-    permanent: true
-  }
+    permanent: true,
+  },
+  {
+    source: '/kursy-dziergania-na-drutach/voucher-kurs-szydelkowy-kotek',
+    destination: '/produkty-do-szydelkowania/voucher-upominkowy',
+    permanent: true,
+  },
+  {
+    source: '/kursy-dziergania-na-drutach/pakiet-materialow-do-warsztatow-druty-ty-tez-to-potrafisz',
+    destination: '/produkty-do-dziergania',
+    permanent: true,
+  },
+  {
+    source: '/landing/10tyg-program-',
+    destination: '/landing/10tyg-program-rozwojowy-dziergania-na-drutach3-edycja2425',
+    permanent: true,
+  },
+  {
+    source: '/l5n/xrqlIHOQicwAAAABJRU5ErkJggg==',
+    destination: '/',
+    permanent: true,
+  },
+  {
+    source: '/produkty-do-dziergania/dodatkowy-pakiet-materialow-do-warsztatow-druty-ty-tez-to-potrafisz',
+    destination: '/produkty-do-dziergania',
+    permanent: true,
+  },
+  {
+    source:
+      '/VCfiMrtovXspFU2tpXvu3Uq1/7k40LQ1HfOtd9PqtF1Q0mmqZEppCe+zJqP8xbGQD/yyaqg3yAA6XWbYAwCLY0O7Aqrat+2C8iozjz3nV9dsDoUdT6Zifkw2WjBOa7DTpUCXpJzISi480ZYVrrioCpFfKdzfb1SAgXke6jVigJBeBl0OA/T6nVleqeC4XVCWNPPZT9Q8FlrFHBLUUkRfWUIkrAKiqSginLzThJXzNSpK8h47a8ra3GLeQbNatiuRmgePggLQsS9CTe5JiNr1bzqEHJGFX/KLR16JJuGVbXlZIdFWMAj3Qi4yTp/EyvPZhHHqOCn+7hiRXj6zbalkDtgE5YcdKsluo1gMTkk1wJF3X9sJNWxUvm/hldZZeKzfOhwyTNTrSZO2hpi4TMLPy0Xm6RPIOXuKUJdyiOxyDmpLfiCmisKkXVB+JOQyPR+wW8CrEgEcO/DWzr8KaqT76kTcqkatYCkVtj2d6/FhssFAaKochQVnkJ19AvkXstHEuUi8IiZ6hEziE5elwnrR0avx0J6liUGIeU0AMVoLTjUF7XZ91kDQ9tVElE7M9TU8nu/141jIQkKaatSwC5C6mINK1jl0C5kkrOGSNj0P24zChQ6HIj/mteydTtmg12cAGPNwYLg18NxIyLtvvM1bPNXln56LNzyd7fbiRIuVxF1yDMq56BIw0U2xsFnNIR0mAYkYBRhUcB/SYm4jLRUcbdQpXw45FABziSgkGz17+zzW3KhFlYxaZFsjTWYy0+Umk202MtygxbhLih0WCqMmAXaYa0iXRUhatDxiqyreVrLy7xu57CZaIvgyoJNlwUhrYHe/z3EoalY30BT3d2c1G/toFZnprCPpdhumGg045FdjP63AXpcEu+0iErNR2G7gE2tVIRHmZ6OMkb9h5ZXZPbKaT2AwSL8ScxqZAbnwhq70ErFWXSZDDXoyG6MxY/hjkwGT9RmgfAcYd4pJprqsFPrEbCIpzCGiglyiKS1etNdwlJDwOz9uNSh9Dn7ZloJxHr2SMjLeYiNzMc8OcKzZiCMNWhzwKDDhlmGClmNfnZRkTMMaLpoq8om06CzKGfl/GLglC9BDW48EFFSvmcv828Qp3G43CUk64sRrmQxbrTjebMTRoA6HfKpnUFqescSYlSLteh7WS0uJjc/Y1FwpWtGWM2YgYtN94RHxumw89oN6GXcr4VFtT0UdOBWxkwwsFdThsF/9r2HcJcZMhl3mGhIx8LBVz3saNgiW6mo5bj2XRf0DJklwZW1er1UAAAAASUVORK5CYII=23:T81a',
+    destination: '/',
+    permanent: true,
+  },
+  {
+    source:
+      '/VCfiMrtovXspFU2tpXvu3Uq1/7k40LQ1HfOtd9PqtF1Q0mmqZEppCe+zJqP8xbGQD/yyaqg3yAA6XWbYAwCLY0O7Aqrat+2C8iozjz3nV9dsDoUdT6Zifkw2WjBOa7DTpUCXpJzISi480ZYVrrioCpFfKdzfb1SAgXke6jVigJBeBl0OA/T6nVleqeC4XVCWNPPZT9Q8FlrFHBLUUkRfWUIkrAKiqSginLzThJXzNSpK8h47a8ra3GLeQbNatiuRmgePggLQsS9CTe5JiNr1bzqEHJGFX/KLR16JJuGVbXlZIdFWMAj3Qi4yTp/EyvPZhHHqOCn+7hiRXj6zbalkDtgE5YcdKsluo1gMTkk1wJF3X9sJNWxUvm/hldZZeKzfOhwyTNTrSZO2hpi4TMLPy0Xm6RPIOXuKUJdyiOxyDmpLfiCmisKkXVB+JOQyPR+wW8CrEgEcO/DWzr8KaqT76kTcqkatYCkVtj2d6/FhssFAaKochQVnkJ19AvkXstHEuUi8IiZ6hEziE5elwnrR0avx0J6liUGIeU0AMVoLTjUF7XZ91kDQ9tVElE7M9TU8nu/141jIQkKaatSwC5C6mINK1jl0C5kkrOGSNj0P24zChQ6HIj/mteydTtmg12cAGPNwYLg18NxIyLtvvM1bPNXln56LNzyd7fbiRIuVxF1yDMq56BIw0U2xsFnNIR0mAYkYBRhUcB/SYm4jLRUcbdQpXw45FABziSgkGz17+zzW3KhFlYxaZFsjTWYy0+Umk202MtygxbhLih0WCqMmAXaYa0iXRUhatDxiqyreVrLy7xu57CZaIvgyoJNlwUhrYHe/z3EoalY30BT3d2c1G/toFZnprCPpdhumGg045FdjP63AXpcEu+0iErNR2G7gE2tVIRHmZ6OMkb9h5ZXZPbKaT2AwSL8ScxqZAbnwhq70ErFWXSZDDXoyG6MxY/hjkwGT9RmgfAcYd4pJprqsFPrEbCIpzCGiglyiKS1etNdwlJDwOz9uNSh9Dn7ZloJxHr2SMjLeYiNzMc8OcKzZiCMNWhzwKDDhlmGClmNfnZRkTMMaLpoq8om06CzKGfl/GLglC9BDW48EFFSvmcv828Qp3G43CUk64sRrmQxbrTjebMTRoA6HfKpnUFqescSYlSLteh7WS0uJjc/Y1FwpWtGWM2YgYtN94RHxumw89oN6GXcr4VFtT0UdOBWxkwwsFdThsF/9r2HcJcZMhl3mGhIx8LBVz3saNgiW6mo5bj2XRf0DJklwZW1er1UAAAAASUVORK5CYII=24:T81a',
+    destination: '/',
+    permanent: true,
+  },
+  {
+    source:
+      'YukrUadO3bULT4SIIuZwv2aw3PK7uWWdLbqYTuq5Dx9S5mqbsHjas0hRh76SK7Ue03BalwhHVYo62aXBuO4SmRdTxuAgHDOMplzcbVle3XMcjHhZLZhcRcRiwvV/xz8tvdstrhA/7X2hoOq6t0aycYVYlAschCgK6TotuxycIQmyvz3mSMZtmzOMR27s1Tw9r1teXvDw98b77xXO2QlCVCj3PotfWaSlVWvUGbcvCc22aDQXTtHBaLlVFR3e6DEdTkvGEx82W97dX/n575f3tL7brLdP+mH8B5U91DpUhPUAAAAAASUVORK5CYII=35:T34b3',
+    destination: '/',
+    permanent: true,
+  },
+  {
+    source: '/kursy-dziergania-na-drutach/materialy-do-bezplatnego-tutorialu-szydelkowe-mini-skarpetki',
+    destination: '/kursy-dziergania-na-drutach',
+    permanent: true,
+  },
+
+  {
+    source: '/kursy-dziergania-na-drutach/markery-knitpro-mindful-chakra',
+    destination: '/produkty-do-dziergania/markery-knitpro-mindful-chakra',
+    permanent: true,
+  },
+  {
+    source: '/kursy-szydelkowania/materialy-do-kursu-nietoperz-karzan',
+    destination: '/kursy-szydelkowania/kurs-nietoperz-karzan-s',
+    permanent: true,
+  },
+  {
+    source: '/kursy-dziergania-na-drutach/druty-knitpro-basix-beech-8mm',
+    destination: '/produkty-do-dziergania',
+    permanent: true,
+  },
+  {
+    source: '/kursy-dziergania-na-',
+    destination: '/kursy-dziergania-na-drutach',
+    permanent: true,
+  },
+  {
+    source: '/kursy-szydelkowania/drukowana-instrukcja-do-kursu-wiewiorka',
+    destination: '/kursy-szydelkowania/kurs-wiewiorka-basia',
+    permanent: true,
+  },
+  {
+    source: '/kursy-szydelkowania/drukowana-instrukcja-do-kursu-torba-jasmin',
+    destination: '/kursy-szydelkowania/kurs-torba-jasmin',
+    permanent: true,
+  },
+  {
+    source: '/kursy-szydelkowania/drukowana-instrukcja-do-kursu-kapelusze-jasmin',
+    destination: '/kursy-szydelkowania/kurs-kapelusz-jasmin',
+    permanent: true,
+  },
+  {
+    source: '/kursy-szydelkowania/drukowana-instrukcja-do-kursu-kapelusz-kotek',
+    destination: '/kursy-szydelkowania/kurs-kapelusz-kotek',
+    permanent: true,
+  },
+  {
+    source: '/kursy-szydelkowania/pakiet-materialow-do-kursu-wiewiorka-basia',
+    destination: '/kursy-szydelkowania/kurs-wiewiorka-basia',
+    permanent: true,
+  },
+  {
+    source: '/kursy-szydelkowania/drukowana-instrukcja-do-kursu-foka',
+    destination: '/kursy-szydelkowania/kurs-foka-dla-zaawansowanych',
+    permanent: true,
+  },
+  {
+    source: '/kursy-szydelkowania/voucher-kurs-szydelkowy-koty-dwa',
+    destination: '/produkty-do-szydelkowania/voucher-upominkowy',
+    permanent: true,
+  },
+  {
+    source: '/landing/warsztaty-druty',
+    destination: '/',
+    permanent: true,
+  },
+  {
+    source: '/kursy-szydelkowania/pakiet-materialow-do-kursu-kapelusz-jasmin',
+    destination: '/kursy-szydelkowania/kurs-kapelusz-jasmin',
+    permanent: true,
+  },
+  {
+    source: '/kursy-szydelkowania/materialy-do-mini-kursu-szydelkowe-scrunchie',
+    destination: '/kursy-szydelkowania',
+    permanent: true,
+  },
+  {
+    source: '/produkty-do-szydelkowania/pakiet-do-kwiatuszkow',
+    destination: '/kursy-szydelkowania',
+    permanent: true,
+  },
+  {
+    source:
+      '/z73127Co5U0bK7NwoPVBfj1pynYWB6H9eujsDqThNvXru68W00nYWV55r8h6fEERF1G+PbQQUiEPNDptcKd2RHYWB6D9dle6KE+h+txM9wai8D1ZBsMNrmgSS+DdqcBPFIhNKhqIeZ3PIMNx5qhWa+E8WgY4v46cFM8UDPydu4QH8HmWhqGE5OAOAn3Fodg6840ICIcfu8NcIsrIeLQQY/XBkGdBCJeC0DUZYZkqB5uDPeBurQIwkop8LO/yTTterg6+9LGzYn992+kDqzPJz+4M93/+u10IuvxxvJzmYGaK0Xw18+LO9v5NbVgFXIAzAWnYDTaAirWuR2rVqv2hWaD/NXxzsCHd+eTJ+4tDEvvzvYHVia7PUuj7YLxiOf7Ri31qUvEfadRJ826Nhh51mfTQNisAgg4TVBHVUK/ywwRh34PLeZ/VCfiMrtovXspFU2tpXvu3Uq1/7k40LQ1HfOtd9PqtF1Q0mmqZEppCe+zJqP8xbGQD/yyaqg3yAA6XWbYAwCLY0O7Aqrat+2C8iozjz3nV9dsDoUdT6Zifkw2WjBOa7DTpUCXpJzISi480ZYVrrioCpFfKdzfb1SAgXke6jVigJBeBl0OA/T6nVleqeC4XVCWNPPZT9Q8FlrFHBLUUkRfWUIkrAKiqSginLzThJXzNSpK8h47a8ra3GLeQbNatiuRmgePggLQsS9CTe5JiNr1bzqEHJGFX/KLR16JJuGVbXlZIdFWMAj3Qi4yTp/EyvPZhHHqOCn+7hiRXj6zbalkDtgE5YcdKsluo1gMTkk1wJF3X9sJNWxUvm/hldZZeKzfOhwyTNTrSZO2hpi4TMLPy0Xm6RPIOXuKUJdyiOxyDmpLfiCmisKkXVB+JOQyPR+wW8CrEgEcO/DWzr8KaqT76kTcqkatYCkVtj2d6/FhssFAaKochQVnkJ19AvkXstHEuUi8IiZ6hEziE5elwnrR0avx0J6liUGIeU0AMVoLTjUF7XZ91kDQ9tVElE7M9TU8nu/141jIQkKaatSwC5C6mINK1jl0C5kkrOGSNj0P24zChQ6HIj/mteydTtmg12cAGPNwYLg18NxIyLtvvM1bPNXln56LNzyd7fbiRIuVxF1yDMq56BIw0U2xsFnNIR0mAYkYBRhUcB/SYm4jLRUcbdQpXw45FABziSgkGz17+zzW3KhFlYxaZFsjTWYy0+Umk202MtygxbhLih0WCqMmAXaYa0iXRUhatDxiqyreVrLy7xu57CZaIvgyoJNlwUhrYHe/z3EoalY30BT3d2c1G/toFZnprCPpdhumGg045FdjP63AXpcEu+0iErNR2G7gE2tVIRHmZ6OMkb9h5ZXZPbKaT2AwSL8ScxqZAbnwhq70ErFWXSZDDXoyG6MxY/hjkwGT9RmgfAcYd4pJprqsFPrEbCIpzCGiglyiKS1etNdwlJDwOz9uNSh9Dn7ZloJxHr2SMjLeYiNzMc8OcKzZiCMNWhzwKDDhlmGClmNfnZRkTMMaLpoq8om06CzKGfl/GLglC9BDW48EFFSvmcv828Qp3G43CUk64sRrmQxbrTjebMTRoA6HfKpnUFqescSYlSLteh7WS0uJjc/Y1FwpWtGWM2YgYtN94RHxumw89oN6GXcr4VFtT0UdOBWxkwwsFdThsF/9r2HcJcZMhl3mGhIx8LBVz3saNgiW6mo5bj2XRf0DJklwZW1er1UAAAAASUVORK5CYII=2f:T81a',
+    destination: '/',
+    permanent: true,
+  },
+  {
+    source:
+      '/Q3PGDszUpmtlPHzBpVjFMnZuzaejZPt2kYs0bM2HQKxmXQMpN6BePUy1nsTRJmvElGm5vkH6h+4QN6SFRNhsXVGJHx4DRqMduth6dNhUmdGFaNEK4WOZytKlgapbDrlXAZNJhsVsBtUMHdooS9SQKLRkTGNJI8NaYQ0GalgJhkD4lJwSdTBjWZ7tASZ4uM2LRCYlHVsdlpUJGnTVLi0MuJy6AmU61KMmNUk6lmObFqRbCoGxizRpSnekQ1dGc9l7QL76NbVE2GVEIMqgQYUPBYLHop/OZeBK2DmOpsglUnwYReigm9BJN6KazaejKq4OOJnMeMyB7mqYY7N2heaTGpvXUVgtslRHz3FqSVpZDdL4NeyMXEYDu2I8+wHwtiyT6MscYGDIq5/9MnvEe6+By01d5mWrhleUpZUULLy4uJuPQqRGW/ENXdm9BVlUHPLUO/uh5L7qfYT0XwJh1FbGka9jY1uvkcdPMq0Cu4i27eHdJZexvGB2WM/t7NPGXkltG9vDukh89BF7+SjEgfYlwjwESzBF5TF9Z8LiQii9he92NrZQF+2wDG9RKYVDyYlLUYEFWRT+IeAYfp5nPyVFPFNbpPUEmGpTV4ImdHQdDci43Zcax7JxBicSDsncCaz4lVrwMh9yie2wbgfqzHSGM96RJVwci/w7TWluepLkEVbW/TkPnhDvjMfWTT68T7dBTHu1EE52yYsfQh4BzBinsUfscgnk2OIBVZxG+HWzhMrOCFx0qmTd2wdj9ixozqPPXCZaETgRmyu+7HYTJEfjtI4vfjNNYCM2jT1KNd/hABSw+SHjOm+h6hQy3AnH0Y77MJ/PF2F692VsnLqB8v1/3MbvR5njpMhem9eIBsRbxIhBdIbNkDn8sEo7IO1wvPo/LaZTja1QhaemBoqAKnuACNgnvwjHYhvGBD5NkEWV9yIrW6wBymQnlqO+qj1567iM9tgsf6mEya2jHQKgOvohg/fHMSl89+hcbaCrSLH6D0yve48O0pVF0vRL9OhOnRDszZ+siicwihhXEmvjydpyL+STq0aCfB+XEE556S5XkrZu2D0ElqUFJ4HreLL8Egq0GrtBoV1y7jZtH30NZXYd4xiPjyDDZDHrK5MotkeJ5JrszlKbell36xYCfJiBfpeIBkU2Gk40EsOE3oa1VipEuHeccQ5uyDMPU8wnCHFotTJuxuBvE2E8PRXpQc7kSQCnuZZddYnpLdL6cHWtVkyWPFzkaAvE5HcZSJsc+8tebDZtiLWGgeG8sebCzPIBH2IptawbvsJt4fJvF2P0a213zwmLqYXnFNnrp65kua89NFYlQIEZizk73kCl7tbuAos4njbAJv9mLIpiLYS4SQ3Y7gaG8D7w4SrOzXgwS7ss85imZ+JVNfXJCnCk9/Thd9fYpwS4pgHeogybUlZFKreJWO4k0mjuP9TVb+eneD5TgTx7tsEu+yCRxnYkjHAmS8pxl1N4qY6sJzeerK6c8/Xj79GSm9dA59LSqyGpjFy/gLVnr4MopX6Q2WT/lgZ509dLQXw5vdKF6n15FY8ZL+Jim4RT8w3IKzOarg5IkPF784wVy/8B3a1A0k6HUisebHTnwZma0I9lOrrDyTirB86vZge5Wt4WA7QmIBD+mU14FTcPbj3R/P/PUfLmHssE/lzD0AAAAASUVORK5CYII=69:T78a,data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAACXBIWXMAAAPoAAAD6AG1e1JrAAAFSElEQVQ4jR2RaVeSCQCF39+WuFuYqblMmaY27sW4sbiA+4JrklEmyiiK4oLIpiAgqLiAQqaZNc7MOfMb5pw5vc8c+XA/3uec+1whRZL0M1mSJKZIkshIlYilBTkMtFQzpqhGWfWc6a5WXJ/GseoGcb7X4nw/wkyXnK76Smqf5VPwKF2UpknITpWIOenJ/wnJkgdiiuSBmJacRL40Q1TUlPKhu5mRliqayov42KMgsDCN1zDFvvEd7o/jGHrbGFfIaK0',
+    destination: '/',
+    permanent: true,
+  },
 ];
